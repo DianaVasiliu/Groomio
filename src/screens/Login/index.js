1a422f9c89fcaeb2a76a21ba568ea3901fc1e0ed
Balketier1 <radu.asblog@gmail.com>
2023-05-21 23:10:49 +0300

    Added login-register screens

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -2,29 +2,86 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Text } from "react-native";
 import { Button } from "native-base";
+import { styles } from "./styles";
 
 import { LoadingIndicator } from "../../components/small";
 import { login } from "../../redux/actions/auth";
 import SafeAreaScreen from "../SafeAreaScreen";
 
-const Login = ({ logIn }) => {
-    const [loading, setLoading] = useState(false);
+import {
+    KeyboardAvoidingView,
+    StyleSheet,
+    View,
+    TextInput,
+    Image,
+} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Logo from "../../../assets/icon.png";
+import { useNavigation } from "@react-navigation/native";
 
-    const loginDefault = async () => {
+const Login = ({ logIn }) => {
+    const navigation = useNavigation();
+    const [loading, setLoading] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const loginUser = async () => {
         setLoading(true);
 
-        // logIn("****", "****").then(() => {
-        //     setLoading(false);
-        // });
+        logIn(email, password).then(() => {
+            setLoading(false);
+        });
     };
 
     return (
-        <SafeAreaScreen>
-            <Text>Login</Text>
+        <KeyboardAvoidingView style={styles.container} behaviour="padding">
+            {/* IMAGE VIEW */}
+            <View>
+                <Image source={Logo} style={styles.logo} resizeMode="contain" />
+            </View>
+            <Text style={styles.title}>Welcome back!</Text>
+            {/* FIELD VIEWS */}
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={text => setEmail(text)}
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                    style={styles.input}
+                    secureTextEntry
+                />
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate("ForgotPassword");
+                    }}>
+                    <Text style={styles.simpleButtonText}>
+                        Forgot password?
+                    </Text>
+                </TouchableOpacity>
+            </View>
 
-            <Button onPress={loginDefault}>Log In with default account</Button>
-            <LoadingIndicator isLoading={loading} />
-        </SafeAreaScreen>
+            {/* BUTTON VIEWS */}
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity onPress={loginUser} style={styles.button}>
+                    <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => {
+                        navigation.navigate("Signup");
+                    }}>
+                    <Text style={styles.simpleText}>
+                        Don't have an account?
+                        <Text> </Text>
+                        <Text style={styles.simpleButtonText}>SignUp</Text>
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
     );
 };
 
--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -1,3 +1,80 @@
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    logo: {
+        maxWidth: 70,
+        maxHeight: 70,
+    },
+
+    title: {
+        marginTop: 10,
+        marginBottom: 100,
+        color: "#151515",
+        fontWeight: 700,
+        fontSize: 32,
+    },
+
+    simpleText: {
+        color: "black",
+        fontWeight: 500,
+        fontSize: 14,
+        marginTop: 35,
+    },
+
+    inputContainer: { width: "80%" },
+
+    input: {
+        backgroundColor: "white",
+        paddingHorizontal: 15,
+        paddingVertical: 15,
+        borderRadius: 20,
+        marginTop: 15,
+    },
+
+    buttonContainer: {
+        width: "60%",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 40,
+    },
+
+    button: {
+        width: "100%",
+        backgroundColor: "#A3C7F1",
+        padding: 15,
+        borderRadius: 10,
+    },
+
+    buttonOutline: {
+        backgroundColor: "white",
+        marginTop: 5,
+        borderColor: "#A3C7F1",
+        borderWidth: 2,
+    },
+
+    buttonText: {
+        color: "white",
+        fontWeight: "700",
+        fontSize: 16,
+    },
+
+    buttonOutlineText: {
+        color: "#A3C7F1",
+        fontWeight: "700",
+        fontSize: 16,
+    },
+
+    simpleButtonText: {
+        color: "#A3C7F1",
+        fontWeight: "700",
+        fontSize: 16,
+        marginTop: 5,
+        alignSelf: "flex-end",
+    },
+});
